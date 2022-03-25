8620de43cdd79b4fae4e5c9d2467a5d14c9abdc8
ABS0N <57865571+ABS0N@users.noreply.github.com>
2022-03-26 00:35:12 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,72 @@
-#ifndef GRAPH_GUARD
-#define GRAPH_GUARD
+/*
+    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images` -std=c++11
+*/
+#include "Simple_window.h"
+#include "Graph.h"
 
-#include "Point.h"
-#include <vector>
-#include "fltk.h"
-#include "std_lib_facilities.h"
+double one(double) { return 1; }
 
-namespace Graph_lib {
+double square(double x) { return x*x; }
 
-#undef major
-#undef minor
+int main()
+{
+    using namespace Graph_lib;
 
-struct Color {
-	enum Color_type {
-		red=FL_RED, blue=FL_BLUE,
-	}
-};
+    int xmax = 1280;
+    int ymax = 720;
 
-class Shape {
-public:
-	void draw() const;}
+    int x_orig = xmax/2;
+    int y_orig = ymax/2;
+
+    int rmin = -11;
+    int rmax = 11;
+    
+    int n_points = 400;
+
+    Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
+
+	Point origo {x_orig, y_orig};
+
+	int xlength = xmax - 40;
+	int ylength = ymax - 40;
+
+	int xscale = 30, yscale = 30;
+
+	Function s (one, rmin, rmax, origo, n_points, xscale, yscale);
+	Function sq (square, rmin, rmax, origo, n_points, xscale, yscale);
+	Function cos_func ( [] (double x) { return cos(x); },
+						rmin, rmax, origo, n_points, xscale, yscale);
+
+	Axis x {Axis::x, Point{20, y_orig}, xlength, xlength/xscale, "x"};
+	Axis y {Axis::y, Point{x_orig, ylength + 20}, ylength, ylength/yscale, "y"};
+
+	Rectangle r {Point{200,200}, 100, 50};
+
+	r.set_fill_color(Color::yellow);
+	r.set_style(Line_style(Line_style::dash, 4));
+
+	Text t {Point{200,400}, "Hello graphics!"};
+	t.set_font(Font::times_bold);
+	t.set_font_size(20);
+
+	Image ii {Point{100,100}, "badge.jpg"};
+
+	Circle c {Point{700,700}, 100};
+
+	Ellipse e {Point{500,500}, 100, 50};
+	e.set_fill_color(Color::red);
+
+	win.attach(e);
+	win.attach(ii);
+	win.attach(c);
+	win.attach(t);
+	win.attach(r);
+	win.attach(s);
+	win.attach(sq);
+	win.attach(cos_func);
+	win.attach(x);
+	win.attach(y);
+
+    win.wait_for_button();
+
 }
-
-Polygon opl = {
-	Point{100,100}, Point{150,200}, {300,200}, {250,250}
-};
-
-win.attach(opl);
-
-win.wait_for_button();
-
-return 0;
-};
